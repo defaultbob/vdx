bb9dcb8ad382a520e2d2f435e9753397886a077f
david.mills <david.mills@veeva.com>
2026-03-16 16:59:23 -0700

    tests

--- patch_utils_3.py
+++
@@ -1,7 +0,0 @@
-with open('test_reassemble.py', 'r') as f:
-    content = f.read()
-
-func_code = content.split('# Generate some dummy data to test')[0].replace('import os\nimport re\n\n', '')
-
-with open('vdx_project/vdx/utils.py', 'a') as f:
-    f.write('\n' + func_code)
